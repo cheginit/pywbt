9c99ac6ec50e851f0551f7cbe72d83d5aba24be2
Taher Chegini <cheginit@gmail.com>
2024-09-02 12:35:56 -0400

    chore: update the tests based on the latest changes to the api.

--- tests/test_pywbt.py
+++ tests/test_pywbt.py
@@ -2,35 +2,34 @@ from __future__ import annotations
 
 import shutil
 import tempfile
+from pathlib import Path
 
 import rasterio
 
-from pywbt import whitebox_tools
+import pywbt
 
 
 def test_whitebox_tools(wbt_zipfile: str) -> None:
-    with tempfile.TemporaryDirectory() as temp_dir:
-        shutil.copy("tests/dem.tif", temp_dir)
-        wbt_args = {
-            "BreachDepressions": [
-                "-i=dem.tif",
-                "--fill_pits",
-                "-o=dem_corr.tif",
-            ],
-            "D8Pointer": ["-i=dem_corr.tif", "-o=fdir.tif"],
-            "D8FlowAccumulation": ["-i=fdir.tif", "--pntr", "-o=streams.tif"],
-        }
-        # To avoid redownloading and hitting the WBT server multiple times for testing
-        # on different platforms and Python versions, especially on CI, the binareis are
-        # stored in `tests/wbt_zip` directory.
-        whitebox_tools(
+    wbt_args = {
+        "BreachDepressions": ["-i=dem.tif", "--fill_pits", "-o=dem_corr.tif"],
+        "D8FlowAccumulation": ["-i=dem_corr.tif", "-o=d8accum.tif"],
+        "ExtractStreams": ["--flow_accum=d8accum.tif", "--threshold=600.0", "-o=streams.tif"],
+    }
+    # To avoid redownloading and hitting the WBT server multiple times for testing
+    # on different platforms and Python versions, especially on CI, the binareis are
+    # stored in `tests/wbt_zip` directory.
+    with tempfile.TemporaryDirectory(suffix="test_", dir=".") as tmpdir:
+        shutil.copy("tests/dem.tif", tmpdir)
+        pywbt.whitebox_tools(
+            tmpdir,
             wbt_args,
-            wbt_root=f"{temp_dir}/wbt",
-            work_dir=temp_dir,
+            ["streams.tif"],
             zip_path=f"tests/wbt_zip/{wbt_zipfile}",
         )
-        with rasterio.open(f"{temp_dir}/streams.tif") as src:
-            assert src.width == 233
-            assert src.height == 303
-            data = src.read(1)
-            assert abs(data.mean() - 3218.5708) < 1e-3
+    with rasterio.open("streams.tif") as src:
+        assert src.width == 233
+        assert src.height == 303
+        data = src.read(1)
+        data[data == src.nodata] = 0
+        assert abs(data.mean() - 0.1243) < 1e-3
+    Path("streams.tif").unlink()
