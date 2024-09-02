a9a24e257ea7b848352d8df73aa86014c4443ccc
Taher Chegini <cheginit@gmail.com>
2024-09-02 12:32:16 -0400

    feat: modify the pywbt api usage to reflect the latest changes and also use the new utils functions.

--- docs/examples/__init__.py
+++ docs/examples/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import annotations
 
-from .utils import get_nasadem, tiff_to_gdf
+from .utils import get_3dep, get_nasadem, tiff_to_da, tiff_to_gdf
 
-__all__ = ["get_nasadem", "tiff_to_gdf"]
+__all__ = ["get_nasadem", "get_3dep", "tiff_to_da", "tiff_to_gdf"]
