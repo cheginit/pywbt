f12da038cf835b0f3ae49b6f80f612b1baa81d71
Taher Chegini <cheginit@gmail.com>
2024-09-19 16:56:45 -0400

    chore: add a new test for wrong zip file.

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def _add_standard_imports(doctest_namespace):
+def add_doctest_imports(doctest_namespace: dict[str, object]) -> None:  # noqa: PT004
     """Add pywbt namespace for doctest."""
     import pywbt
 
@@ -29,4 +29,21 @@ def wbt_zipfile() -> str:
         suffix = "darwin_m_series" if platform.machine() == "arm64" else "darwin_amd64"
     else:
         suffix = "linux_musl" if "musl" in platform.libc_ver()[0].lower() else "linux_amd64"
+    return base_name.format(suffix)
+
+
+@pytest.fixture
+def wrong_wbt_zipfile() -> str:
+    """Determine the platform suffix for downloading WhiteboxTools."""
+    system = platform.system()
+    base_name = "WhiteboxTools_{}.zip"
+    if system not in ("Windows", "Darwin", "Linux"):
+        raise ValueError(f"Unsupported operating system: {system}")
+
+    if system == "Windows":
+        suffix = "darwin_amd64"
+    elif system == "Darwin":
+        suffix = "linux_amd64"
+    else:
+        suffix = "win_amd64"
     return base_name.format(suffix)
