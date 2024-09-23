2fbc25fea951cf296d745477d18455a8c98a0fc5
Taher Chegini <cheginit@gmail.com>
2024-09-22 21:42:40 -0400

    feat: add a new module containing function for getting dem data.

--- src/pywbt/__init__.py
+++ src/pywbt/__init__.py
@@ -5,10 +5,11 @@ from __future__ import annotations
 from importlib.metadata import PackageNotFoundError, version
 
 from pywbt.pywbt import list_tools, tool_parameters, whitebox_tools
+from pywbt import dem_utils
 
 try:
     __version__ = version("pywbt")
 except PackageNotFoundError:
     __version__ = "999"
 
-__all__ = ["whitebox_tools", "list_tools", "tool_parameters"]
+__all__ = ["whitebox_tools", "list_tools", "tool_parameters", "dem_utils"]
