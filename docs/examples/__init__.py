24985a408b39f1a02050cc3b8e95e1d302fd09d1
Taher Chegini <cheginit@gmail.com>
2024-09-24 15:00:31 -0400

    feat: use the new dem module.

--- docs/examples/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Make the utils module available to the user."""
-
-from __future__ import annotations
-
-from .utils import get_3dep, get_nasadem, tiff_to_da, tiff_to_gdf
-
-__all__ = ["get_nasadem", "get_3dep", "tiff_to_da", "tiff_to_gdf"]
