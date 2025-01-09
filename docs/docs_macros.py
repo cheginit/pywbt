b378a11f36f85861ec8dfb5d0974e2fe0513f9f6
Taher Chegini <cheginit@gmail.com>
2025-01-09 11:42:56 -0500

    chore: run pre-commit.

--- docs/docs_macros.py
+++ docs/docs_macros.py
@@ -1,10 +1,16 @@
-def define_env(env):
-    """Define variables and macros for the site"""
-    def issue_link(number):
-        return f'[#{number}](https://github.com/cheginit/pywbt/issues/{number})'
-    
-    def commit_link(hash):
-        return f'[{hash[:7]}](https://github.com/cheginit/pywbt/commit/{hash})'
+"""Module to define variables and macros for the site."""
 
-    env.macro(issue_link, 'issue')
-    env.macro(commit_link, 'commit')
+from __future__ import annotations
+
+
+def define_env(env):
+    """Define variables and macros for the site."""
+
+    def issue_link(number):
+        return f"[#{number}](https://github.com/cheginit/pywbt/issues/{number})"
+
+    def commit_link(hash):
+        return f"[{hash[:7]}](https://github.com/cheginit/pywbt/commit/{hash})"
+
+    env.macro(issue_link, "issue")
+    env.macro(commit_link, "commit")
